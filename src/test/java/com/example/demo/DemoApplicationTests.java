80a53e18ecb8e9b33c5efc74fdb2f939649ebb36
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2017-09-09 15:17:36 -0300

    Aula 12 - Configurando os testes de integração HTTP no Spring

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,25 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Sql(value = "/load-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
 public class DemoApplicationTests {
+
+	@Value("${local.server.port}")
+	protected int porta;
 
 	@Test
 	public void contextLoads() {
+		System.out.println(porta);
 	}
 
 }
